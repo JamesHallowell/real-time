74fe80f0e6856a22f4c4a2a60ecd0f65f5053455
James Hallowell <5460955+JamesHallowell@users.noreply.github.com>
2024-10-27 14:25:43 +0000

    Add cursor caching

---
+++ benches/fifo.rs
@@ -0,0 +1,52 @@
+use {
+    criterion::{black_box, criterion_group, criterion_main, Bencher, Criterion},
+    real_time::fifo,
+};
+
+fn write(bencher: &mut Bencher) {
+    let (writer, _reader) = fifo::fifo::<_, 128>();
+
+    bencher.iter(|| writer.push(1));
+}
+
+fn read(bencher: &mut Bencher) {
+    let (writer, reader) = fifo::fifo::<_, 128>();
+
+    writer.push_blocking(1);
+
+    bencher.iter(|| {
+        black_box(reader.pop_ref());
+    });
+}
+
+fn read_empty(bencher: &mut Bencher) {
+    let (_writer, reader) = fifo::fifo::<i32, 128>();
+
+    bencher.iter(|| {
+        black_box(reader.pop_ref());
+    });
+}
+
+fn read_multi(bencher: &mut Bencher) {
+    let (writer, reader) = fifo::fifo::<_, 128>();
+
+    for value in 0..128 {
+        writer.push_blocking(value);
+    }
+
+    bencher.iter(|| {
+        while let Some(value) = reader.pop_ref() {
+            black_box(value);
+        }
+    });
+}
+
+fn fifo_benchmark(c: &mut Criterion) {
+    c.bench_function("write", write);
+    c.bench_function("read", read);
+    c.bench_function("read_empty", read_empty);
+    c.bench_function("read_multi", read_multi);
+}
+
+criterion_group!(benches, fifo_benchmark);
+criterion_main!(benches);
